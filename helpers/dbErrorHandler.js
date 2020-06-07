9c824fb767aca19af5f2391837f243fabfb197c8
askariabidi <smohammadaskariabidi@khi.iba.edu.pk>
2020-06-07 21:35:37 +0500

    7-Jun-2020

--- helpers/dbErrorHandler.js
+++ helpers/dbErrorHandler.js
@@ -3,7 +3,7 @@
 /**
  * Get unique error field name
  */
-const uniqueMessage = error => {
+const uniqueMessage = error => { 
     let output;
     try {
         let fieldName = error.message.substring(
