d74131dba1f3a22c9550632c2504724fedfaea22
askariabidi <smohammadaskariabidi@khi.iba.edu.pk>
2020-06-07 23:47:40 +0500

    7-Jun-2020

--- models/product.js
+++ models/product.js
@@ -11,7 +11,7 @@ const productSchema = new mongoose.Schema(
             maxlength: 32
         },
         isbn: {
-            type: Number,
+            type: String,
             required: true
         },
         publisher: {
