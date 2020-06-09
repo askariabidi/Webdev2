bc3c060225d57bac23792407ccdf37c0a0f0b7df
askariabidi <smohammadaskariabidi@khi.iba.edu.pk>
2020-06-09 11:38:29 +0500

    9-Jun-2020

--- validator/index.js
+++ validator/index.js
@@ -18,5 +18,5 @@ exports.userSignupValidator = (req, res, next) => {
         const firstError = errors.map(error => error.msg)[0];
         return res.status(400).json({ error: firstError });
     }
-    next();
+    next();  
 };
